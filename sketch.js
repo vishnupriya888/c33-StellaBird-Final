a8502be038f41726d43c47428b4704b5f777b038
vishnupriya-whitehatjr <whitehatjr.priya@gmail.com>
2020-04-11 19:40:49 +0530

    commit

--- sketch.js
+++ sketch.js
@@ -1,13 +1,19 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
-var box1, pig1;
-var backgroundImg;
+var box1, pig1,pig3;
+var backgroundImg,platform;
+var bird, slingshot;
+
+var gameState = "onSling";
+var bg = "images/background.jpg";
+var score = 0;
 
 function preload() {
-    backgroundImg = loadImage("images/background.jpg");
+    getBackgroundImg();
 }
 
 function setup(){
@@ -15,8 +21,9 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
+    platform = new Platform(150, 305, 300, 135);
+    ground = new Ground(600,height,1200,100);
     
-    ground = new Ground(600,height,1200,100)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -33,25 +40,34 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(200,50);
 
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
 function draw(){
-    background(backgroundImg);
+    if(backgroundImg)
+        background(backgroundImg);
+    
+        noStroke();
+        textSize(35)
+        fill("white")
+        text("Score : " + score, width-300, 50)
+    
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -59,4 +75,46 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
+    //log6.display();
+    slingshot.display();
+    console.log(bird.body.speed);    
+}
+
+function mouseDragged(){
+    //if (gameState!=="launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    //}
+}
+
+
+function mouseReleased(){
+    slingshot.fly();
+    gameState = "launched";
+}
+
+function keyPressed(){
+    if(keyCode === 32 && bird.body.speed < 1){
+       bird.trajectory = [];
+       Matter.Body.setPosition(bird.body,{x:200, y:50});
+       slingshot.attach(bird.body);
+    }
+}
+
+async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
+
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+    
+    if(hour>=0600 && hour<=1900){
+        bg = "images/background.jpg";
+    }
+    else{
+        bg = "images/background.jpg";
+    }
+
+    backgroundImg = loadImage(bg);
+    console.log(backgroundImg);
 }
